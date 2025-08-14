8bb0578978fda412e39eda5bca2a15a3a3831a3b
DevRobertoMartinez <217862081+DevRobertoMartinez@users.noreply.github.com>
2025-08-13 22:52:41 -0400

    Adds title to README

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# initializing wallpaper daemon
+swww-daemon &
+
+# setting wallpaper
+swww img ~/.dotfiles/assets/Wallpapers/lofiwallpaper.png &
+
+# you can install this by adding 
+# pkgs.networkmanagerapplet to your packages
+nm-applet --indicator &
+
+
+# the bar
+waybar &
+
+# dunst
+dunst &
